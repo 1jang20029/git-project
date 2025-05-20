901eebdbbcc266b56c8450a7f859e0536c7b10aa
1jang20029 <groria123@naver.com>
2025-05-20 17:58:27 +0900

    전체 파일 업데이트

--- student-deals.js
+++ student-deals.js
@@ -811,7 +811,7 @@ document.addEventListener('DOMContentLoaded', function() {
                         saveUserInteractions();
                         
                         // 알림 표시
-                        alert('삭제가 완료되었습니다.');
+                        alert('${selectedRestaurant.name} 삭제가 완료되었습니다.');
                         
                         // 목록으로 돌아가기
                         restaurantDetail.classList.add('hidden');
