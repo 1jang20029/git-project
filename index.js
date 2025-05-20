db196e46dec76132e24a62b95d542595284c1502
1jang20029 <groria123@naver.com>
2025-05-21 02:14:38 +0900

    전체 파일 업데이트

--- index.js
+++ index.js
@@ -5171,7 +5171,7 @@ function loadPopularRestaurants() {
                 stars: 5,
                 dislikes: 0,
                 images: [
-                    'GGgo-yeonsung.jpg'
+                    'images/GGgo-yeonsung.jpg'
                 ]
             },
             {
