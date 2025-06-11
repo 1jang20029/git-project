de3bd6b3037b0aa16d708879b6009584a5b880f8
1jang20029 <groria123@naver.com>
2025-06-11 23:27:30 +0900

    전체 파일 업데이트

--- front/js/index.js
+++ front/js/index.js
@@ -218,7 +218,7 @@ function showContent(type) {
 
   if (type === 'settings' && !settingsLoaded) {
     const container = document.getElementById('settingsContent');
-    fetch('settings.html')
+    fetch('pages/user/settings.html')
       .then(res => {
         if (!res.ok) throw new Error('settings.html 을 불러오는 중 오류 발생');
         return res.text();
