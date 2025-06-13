f4ae1604d35a131e6db9898499e90dbfa2366a02
1jang20029 <groria123@naver.com>
2025-06-13 22:30:24 +0900

    전체 파일 업데이트

--- front/js/index/modules/utils.js
+++
@@ -1,8 +0,0 @@
-// js/index/modules/utils.js
-
-export function fmtTime(minutes, suffix) {
-  if (minutes < 60) return `${minutes}분 ${suffix}`;
-  const h = Math.floor(minutes / 60);
-  const m = minutes % 60;
-  return m === 0 ? `${h}시간 ${suffix}` : `${h}시간 ${m}분 ${suffix}`;
-}
